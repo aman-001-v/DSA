25472b88ac939b40d5c3d459671c147b74dd5995
aman-001-v <a0110verma@gmail.com>
2025-12-29 04:03:04 +0000

    [LeetCode Sync] Runtime - 13 ms (90.42%), Memory - 60 MB (81.49%)

---
+++ leetcode/0817-design-hashmap/solution.cpp
@@ -0,0 +1,31 @@
+class MyHashMap {
+public:
+    unordered_map<int, int> table;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        table[key] = value;
+    }
+    
+    int get(int key) {
+        if(table.find(key) == table.end()){
+            return -1;
+        }else{
+            return table[key];
+        }
+    }
+    
+    void remove(int key) {
+        table.erase(key);
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
