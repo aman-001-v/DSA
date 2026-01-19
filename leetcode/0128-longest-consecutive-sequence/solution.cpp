822f925b0c2f411a1b83d89f72fa3cc5cc458545
aman-001-v <a0110verma@gmail.com>
2026-01-19 12:37:09 +0000

    [LeetCode Sync] Runtime - 80 ms (60.29%), Memory - 89 MB (49.94%)

--- leetcode/0128-longest-consecutive-sequence/solution.cpp
+++ leetcode/0128-longest-consecutive-sequence/solution.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size() == 0 ) return 0;
-        sort(nums.begin(), nums.end());
-        int count = 1;
-        int maxC = 0;
-        for(int i = 1; i< nums.size() ; i++){
-            cout << nums[i] << endl;
-            if(nums[i] == nums[i-1] + 1 ){
-                
-                count++;
-            }
-            else if(nums[i] == nums[i-1]) continue;
-            else{
-                if(maxC < count) maxC = count;
-                count = 1;
-            }
-            
+        unordered_set <int> memo;
+        for(const auto q: nums){
+            memo.insert(q);
         }
-        if(maxC < count) maxC = count;
-        return maxC;
+        cout << "nuaugf" << endl;
+        int longest = 0;
+        for(const auto q: memo){
+            if(memo.find(q-1) == memo.end()){
+                int length = 0;
+                while(memo.find(q + length) != memo.end()){
+                    length++;
+                } 
+                if(length > longest) longest = length;
+            }
+        }
+        return longest;
     }
 };
