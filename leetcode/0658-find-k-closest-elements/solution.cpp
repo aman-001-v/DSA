efc1134b1fa1ad127c51f59ab8e41862643a3210
aman-001-v <a0110verma@gmail.com>
2026-02-11 14:57:56 +0000

    [LeetCode Sync] Runtime - 2 ms (56.01%), Memory - 35.9 MB (64.97%)

--- leetcode/0658-find-k-closest-elements/solution.cpp
+++ leetcode/0658-find-k-closest-elements/solution.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n = arr.size();
-        int left = 0, right = n - k;
-        
-        // Binary search for the best starting index of window size k
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            // Compare distance from x to arr[mid] and arr[mid + k]
-            if (x - arr[mid] > arr[mid + k] - x) {
-                // Better window is on the right
-                left = mid + 1;
-            } else {
-                // Better window is on the left or current
-                right = mid;
+        int left = 0;
+        int right = n - 1;
+        while(left < right && right - left + 1 > k){
+            if(abs(arr[left] - x) <= abs(arr[right] - x)){
+                right--;
+            }
+            else if(abs(arr[left] - x) > abs(arr[right] - x)){
+                left++;
             }
         }
-        
-        // left is the best starting index
         return vector<int>(arr.begin() + left, arr.begin() + left + k);
     }
 };
