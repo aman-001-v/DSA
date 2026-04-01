79561b73cea75912b8926532f7242ccee0f5ae6b
aman-001-v <a0110verma@gmail.com>
2026-04-01 18:04:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (5.44%)

---
+++ leetcode/0374-guess-number-higher-or-lower/solution.cpp
@@ -0,0 +1,30 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        if(guess(n) == 0) return n;
+        int mid = (1+n)/2;
+        while(true){
+            int g = guess(mid);
+            if(g == 0){
+                return mid;
+            }
+            else if(g == -1){
+                n = mid - 1;
+            }
+            else if(g == 1){
+                n = mid + 1;
+            }
+            mid = (1+n)/2;
+        }
+        return mid;
+    }
+};
