6785e7337b4fe8819e1d55a8a0260911266bd1af
aman-001-v <a0110verma@gmail.com>
2026-04-04 13:34:54 +0000

    [LeetCode Sync] Runtime - 4 ms (0.55%), Memory - 11.4 MB (86.35%)

---
+++ leetcode/0066-plus-one/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size() - 1;
+        while(n >= 0 && digits[n] == 9){
+            digits[n] = 0;
+            n--;
+        }
+        if(n >= 0){
+            digits[n] +=1;
+        }
+        else{
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
